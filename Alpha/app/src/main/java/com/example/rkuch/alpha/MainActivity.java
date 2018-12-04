c624e3b0b61144e21ef32a49a6166e7a74e8e756
Ricardo Kuchimpos <rkuchimpos@live.com>
2018-12-03 17:46:48 -0800

    Get current location on phone

--- Alpha/app/src/main/java/com/example/rkuch/alpha/MainActivity.java
+++ Alpha/app/src/main/java/com/example/rkuch/alpha/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.rkuch.alpha;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    //final Fragment locationFragment = new LocationFragment();
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    LocationManager locationManager;
+    Location currentLocation;
+
     final Fragment locationFragment = new MapsFragment();
     final Fragment historyFragment = new HistoryFragment();
     final Fragment alertsFragment = new AlertsFragment();
@@ -50,6 +61,44 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager.beginTransaction().add(R.id.main_container, alertsFragment, "3").hide(alertsFragment).commit();
         fragmentManager.beginTransaction().add(R.id.main_container, historyFragment, "2").hide(historyFragment).commit();
         fragmentManager.beginTransaction().add(R.id.main_container, locationFragment, "1").commit();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    99);
+        }
+
+        getLocation();
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLocation = location;
+        // DEBUG:
+        Toast.makeText(getApplicationContext(), "ENTRY: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Alpha/app/src/main/java/com/example/rkuch/alpha/MapsFragment.java
+++ Alpha/app/src/main/java/com/example/rkuch/alpha/MapsFragment.java
@@ -1,5 +1,6 @@
 package com.example.rkuch.alpha;
 
+import android.location.LocationListener;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,34 +16,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
-
-    /*
-    private GoogleMap mMap;
-
-    public MapsFragment() {
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_maps, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        return view;
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-    */
     private MapView mapView;
     private GoogleMap mMap;
 
